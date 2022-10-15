b2fa1afdcbe5c98494ae2d37796822259dbb64fe
Tatiana <tt_tanya_88@mail.ru>
2022-10-15 14:00:49 +0300

    dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите точку k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите точку k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите точку b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите точку b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+double x = (b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Прямые пересекуться в точке: ({x};{y})");
